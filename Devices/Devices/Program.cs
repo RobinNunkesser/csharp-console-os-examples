31b0988ba9cf109234c1bf9328bbbe2977123f26
Robin Nunkesser <robin.nunkesser@hshl.de>
2020-11-12 17:18:48 +0100

    New project

---
+++ Devices/Devices/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace Devices
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using var deviceWriter =
+                new StreamWriter("/dev/stdout") {AutoFlush = true};
+            using var deviceReader = new StreamReader("/dev/stdout");
+            deviceWriter.WriteLine("Wie heißen Sie? ");
+            var input = deviceReader.ReadLine();
+            deviceWriter.WriteLine($"Hallo {input}");
+            deviceReader.Close();
+            deviceWriter.Close();
+        }
+    }
+}
